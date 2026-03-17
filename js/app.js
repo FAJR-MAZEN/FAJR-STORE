13bc6304dc4f2d104375391bdc424589f3b972f9
FAJR-MAZEN <fajr38799@gmail.com>
2026-03-17 04:43:49 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
+const products = [
 
+{
+name:"FAJR ENERGY",
+price:"30 SAR",
+image:"images/fajr-energy.png"
+}
+
+];
+
+const container = document.getElementById("products");
+
+products.forEach(product => {
+
+const card = document.createElement("div");
+
+card.className = "product";
+
+card.innerHTML = `
+<img src="${product.image}" width="200">
+<h3>${product.name}</h3>
+<p>${product.price}</p>
+<button>شراء</button>
+`;
+
+container.appendChild(card);
+
+});
